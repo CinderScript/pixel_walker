dada9a60ed7743620819a2cf4aa21dc3b9043f56
nguyent60 <nguyent60@spu.edu>
2022-05-18 16:24:57 -0700

    FileBasedPrompt
    Handle Prompt from file
    Send Prompt To GPT-3 Handler

---
+++ GPT-3Handler/FIleBasedPrompt/Prompt.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+namespace System.Text.Json
+
+{
+    public class GPTPrompt
+    {
+        public string ClassifyResponsePrompt { get; set; }
+        public string GetQuestionAnswerPrompt { get; set; }
+        public string ParseCommandPrompt { get; set; }
+    }
+
+    public class Program
+    {
+        static void Main()
+        {
+            StreamReader r = new StreamReader("C:/Users/Tn/Documents/ReadPrompts/Prompt.json");
+            string jsonString = r.ReadToEnd();
+            GPTPrompt Prompt = JsonSerializer.Deserialize<GPTPrompt>(jsonString);
+            Console.WriteLine(Prompt.ClassifyResponsePrompt);
+            Console.ReadKey();
+        }
+    }
+}
