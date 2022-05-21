6c5bfc0442d1b66fc1bda2f097fd79ac7e1dc018
nguyent60 <77171826+nguyent60@users.noreply.github.com>
2022-05-20 21:24:13 -0700

    Add files via upload

---
+++ GPT-3Handler/Prompt.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+namespace System.Text.Json
+
+{
+    public class GPTPrompt
+    {
+        public string ClassifyResponsePrompt { get; set; }
+        public string GetQuestionAnswerPrompt { get; set; }
+        public string ParseCommandPrompt { get; set; }
+
+        public GPTPrompt GetPrompt()
+        {
+            GPTPrompt Prompt;
+            StreamReader r = new StreamReader("C:/Users/Tn/Documents/ReadPrompts/Prompt.json");
+            string jsonString = r.ReadToEnd();
+            Prompt = JsonSerializer.Deserialize<GPTPrompt>(jsonString);
+            ClassifyResponsePrompt = Prompt.ClassifyResponsePrompt;
+            GetQuestionAnswerPrompt = Prompt.GetQuestionAnswerPrompt;
+            ParseCommandPrompt = Prompt.ParseCommandPrompt;
+            return Prompt;
+        }
+    }
+
+    public class Program
+    {
+        static void Main()
+        {
+            GPTPrompt example = new GPTPrompt();
+            example.GetPrompt();
+            Console.WriteLine($"{example.ClassifyResponsePrompt}");
+        }
+    }
+}
