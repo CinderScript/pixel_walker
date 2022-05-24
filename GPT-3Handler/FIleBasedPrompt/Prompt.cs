ccda34eaf85272a23813634a7657c6785732b037
nguyent60 <nguyent60@spu.edu>
2022-05-24 03:08:04 -0700

    Changes in the Prompt Loader method
    Re-commit to make changes to the Prompt Loader method.

--- GPT-3Handler/FIleBasedPrompt/Prompt.cs
+++ GPT-3Handler/FIleBasedPrompt/Prompt.cs
@@ -1,35 +1,52 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using System.IO;
+using System.Threading.Tasks;
 using Newtonsoft.Json;
-namespace System.Text.Json
 
+public class PromptLoader
 {
-    public class GPTPrompt
+
+    //NOTE: FilePrompts not implemented yet,
+    //thus using hard coded classifier variables for testing purposes - Shub
+    public GptPrompts GetPromptsFromFile(string filePath)
     {
-        public string ClassifyResponsePrompt { get; set; }
-        public string GetQuestionAnswerPrompt { get; set; }
-        public string ParseCommandPrompt { get; set; }
 
-        public GPTPrompt GetPrompt()
-        {
-            GPTPrompt Prompt;
-            StreamReader r = new StreamReader("C:/Users/Tn/Documents/ReadPrompts/Prompt.json");
-            string jsonString = r.ReadToEnd();
-            Prompt = JsonSerializer.Deserialize<GPTPrompt>(jsonString);
-            ClassifyResponsePrompt = Prompt.ClassifyResponsePrompt;
-            GetQuestionAnswerPrompt = Prompt.GetQuestionAnswerPrompt;
-            ParseCommandPrompt = Prompt.ParseCommandPrompt;
-            return Prompt;
-        }
+        StreamReader r = new StreamReader(filePath);
+        string jsonString = r.ReadToEnd();
+
+        GptPrompts prompts;
+        // TODO: load json from file, deserialize json into Prompts object
+        prompts = JsonConvert.DeserializeObject<GptPrompts>(jsonString);
+        GptPrompts output = new GptPrompts(prompts.InputClassifier, prompts.QuestionResponder, prompts.CommandParser, prompts.BestMatchSelector);
+        return output;
     }
+}
 
-    public class Program
+public class GptPrompts
+{
+    public string InputClassifier { get; }
+    public string QuestionResponder { get; }
+    public string CommandParser { get; }
+    public string BestMatchSelector { get; }
+
+    public GptPrompts(string inputClassifier, string questionResponder, string commandParser, string bestMatchSelector)
+    {
+        InputClassifier = inputClassifier;
+        QuestionResponder = questionResponder;
+        CommandParser = commandParser;
+        BestMatchSelector = bestMatchSelector;
+    }
+}
+public class Program
     {
         static void Main()
         {
-            GPTPrompt example = new GPTPrompt();
-            example.GetPrompt();
-            Console.WriteLine($"{example.ClassifyResponsePrompt}");
+            PromptLoader example = new PromptLoader();
+            GptPrompts prompts = new GptPrompts("","","","");
+            prompts = example.GetPromptsFromFile("C:/Users/Tn/Documents/ReadPrompts/Prompt.json");
+            Console.WriteLine($"{prompts.InputClassifier}");
         }
     }
-}
